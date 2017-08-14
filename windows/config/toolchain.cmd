fcb4821a06435558bed6a87d50336ab8067c7162
Ben Fu <ben_fu1@yahoo.com>
2017-08-14 02:24:51 -0500

    Add vswhere for locating VS2017

--- windows/config/toolchain.cmd
+++ windows/config/toolchain.cmd
@@ -1,16 +1,20 @@
 @echo off
+
 SET TOOLCHAIN32=""
 SET TOOLCHAIN64=""
 SET TOOLCHAINARM=""
 
-IF "%VSVERSION%" == "15" (
-  
-  SET TOOLCHAIN32="%VS150COMNTOOLS%..\..\VC\bin\vcvars32.bat"
-  SET TOOLCHAIN64="%VS150COMNTOOLS%..\..\VC\bin\amd64\vcvars64.bat"
-  SET TOOLCHAINARM="%VS150COMNTOOLS%..\..\VC\bin\x86_arm\vcvarsx86_arm.bat"
-  SET TOOLCHAIN_NAME=Visual Studio 15 2017
+SET CWD=%~dp0
+for /f "usebackq tokens=*" %%i in (`%CWD%\vswhere.exe -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set VS2017DIR=%%i
 )
 
+IF "%VSVERSION%" == "15" (
+  SET TOOLCHAIN32="%VS2017DIR%\VC\Auxiliary\Build\vcvars32.bat"
+  SET TOOLCHAIN64="%VS2017DIR%\VC\Auxiliary\Build\vcvars64.bat"
+  SET TOOLCHAINARM="%VS2017DIR%\VC\Auxiliary\Build\vcvarsx86_arm.bat"
+  SET TOOLCHAIN_NAME=Visual Studio 15 2017
+)
 IF "%VSVERSION%" == "14" (
   SET TOOLCHAIN32="%VS140COMNTOOLS%..\..\VC\bin\vcvars32.bat"
   SET TOOLCHAIN64="%VS140COMNTOOLS%..\..\VC\bin\amd64\vcvars64.bat"
