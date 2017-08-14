a61a86c0295eae898c3d5261a688ac6abc2b4f2a
Ben Fu <ben_fu1@yahoo.com>
2017-08-13 23:31:47 -0500

    Add support for VS 2017

--- windows/config/toolchain.cmd
+++ windows/config/toolchain.cmd
@@ -3,6 +3,14 @@ SET TOOLCHAIN32=""
 SET TOOLCHAIN64=""
 SET TOOLCHAINARM=""
 
+IF "%VSVERSION%" == "15" (
+  "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+  SET TOOLCHAIN32="%VS150COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+  SET TOOLCHAIN64="%VS150COMNTOOLS%..\..\VC\bin\amd64\vcvars64.bat"
+  SET TOOLCHAINARM="%VS150COMNTOOLS%..\..\VC\bin\x86_arm\vcvarsx86_arm.bat"
+  SET TOOLCHAIN_NAME=Visual Studio 15 2017
+)
+
 IF "%VSVERSION%" == "14" (
   SET TOOLCHAIN32="%VS140COMNTOOLS%..\..\VC\bin\vcvars32.bat"
   SET TOOLCHAIN64="%VS140COMNTOOLS%..\..\VC\bin\amd64\vcvars64.bat"
